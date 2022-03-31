6f280d10485b7938493d2adbee41f344053ef54d
Jeskeren <100347068+Jeskeren@users.noreply.github.com>
2022-03-31 08:40:23 +0800

    Update sibuk.py

--- userbot/modules/sibuk.py
+++ userbot/modules/sibuk.py
@@ -65,8 +65,7 @@ async def set_afk(afk_e):
         AFKREASON = string
         await afk_e.edit(f"{REPO_NAME}\n╭✠╼━━━━━━❖━━━━━━━✠╮\n┣ {ALIVE_NAME} __𝐋𝐚𝐠𝐢 𝐀𝐅𝐊__\n┣ ᴀʟᴀꜱᴀɴ : {AFKREASON}\n╰✠╼━━━━━━❖━━━━━━━✠╯")
     else:
-        await afk_e.edit("🃏JESS𝙆\n╭✠╼━━━━━━❖━━━━━━━
-✠╮ \n┣ __𝐋𝐚𝐠𝐢 𝐀𝐅𝐊__ \n╰✠╼━━━━━━❖━━━━━━━✠╯")
+        await afk_e.edit("💀 𝘼 𝙁 𝙆\n╭✠╼━━━━━━❖━━━━━━━✠╮ \n┣ __𝐋𝐚𝐠𝐢 𝐀𝐅𝐊__ \n╰✠╼━━━━━━❖━━━━━━━✠╯")
     if user.last_name:
         await afk_e.client(UpdateProfileRequest(first_name=user.first_name, last_name=user.last_name + " ⚠️𝐀 𝐅 𝐊⚠️ "))
     else:
@@ -91,7 +90,7 @@ async def type_afk_is_not_true(notafk):
     global afk_end
     user = await bot.get_me()  # pylint:disable=E0602
     last = user.last_name
-    if last and last.endswith(" ⚠️JESS⚠️ "):
+    if last and last.endswith(" ⚠️𝐀 𝐅 𝐊⚠️ "):
         last1 = last[:-12]
     else:
         last1 = ""
@@ -99,7 +98,7 @@ async def type_afk_is_not_true(notafk):
     afk_end = back_alive.replace(microsecond=0)
     if ISAFK:
         ISAFK = False
-        msg = await notafk.respond("🃏JESS")
+        msg = await notafk.respond("💀𝐎𝐍𝐋𝐈𝐍𝐄")
         time.sleep(3)
         await msg.delete()
         await notafk.client(UpdateProfileRequest(first_name=user.first_name, last_name=last1))
